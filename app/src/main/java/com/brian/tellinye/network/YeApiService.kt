df89f8126c8aceaef60446d122f6a8a9a119b7c3
bryanares <bryanares12@gmail.com>
2022-09-24 11:02:30 +0300

    initial commit

---
+++ app/src/main/java/com/brian/tellinye/network/YeApiService.kt
@@ -0,0 +1,7 @@
+package com.brian.tellinye.network
+
+//base url
+private const val BASE_URL = "https://api.kanye."
+
+
+//build Moshi object with Kotlin adapter factory for Retrofit to to parse the JSON
