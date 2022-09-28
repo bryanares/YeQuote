96621dd468718f9e56bf628dd6576d2493343c3b
bryanares <bryanares12@gmail.com>
2022-09-28 10:01:19 +0300

    force light mode on test device

--- app/src/main/java/com/brian/tellinye/MainActivity.kt
+++ app/src/main/java/com/brian/tellinye/MainActivity.kt
@@ -2,9 +2,12 @@ package com.brian.tellinye
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        //force light mode on physical testing device
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
