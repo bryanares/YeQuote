6b98f38d9c78d4a22eefef50fb035f4aeda58e85
bryanares <bryanares12@gmail.com>
2022-09-28 09:39:15 +0300

    load quote on startup

--- app/src/main/java/com/brian/tellinye/ui/SplashFragment.kt
+++ app/src/main/java/com/brian/tellinye/ui/SplashFragment.kt
@@ -5,12 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.brian.tellinye.R
 import com.brian.tellinye.databinding.FragmentSplashBinding
 
 
 class SplashFragment : Fragment() {
+    private val viewModel : YeViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +20,9 @@ class SplashFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val binding = FragmentSplashBinding.inflate(inflater, container, false)
+
+        //load quote
+        viewModel.getYeQuote()
 
         binding.startButton.setOnClickListener {
             findNavController().navigate(R.id.action_splashFragment_to_quotesFragment2)
