bcf55b0987f56a565d3246d95be72d50bf94e7c7
bryanares <bryanares12@gmail.com>
2022-10-01 13:28:06 +0300

    clear unused imports

--- app/src/main/java/com/brian/tellinye/ui/QuotesFragment.kt
+++ app/src/main/java/com/brian/tellinye/ui/QuotesFragment.kt
@@ -5,10 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import com.brian.tellinye.databinding.FragmentQuotesBinding
-import com.brian.tellinye.network.Ye
 
 
 class QuotesFragment : Fragment() {
@@ -17,20 +15,22 @@ class QuotesFragment : Fragment() {
 //    private var _binding: FragmentQuotesBinding? = null
 //    private val binding get() = _binding!!
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
-      val binding = FragmentQuotesBinding.inflate(inflater, container, false)
+        val binding = FragmentQuotesBinding.inflate(inflater, container, false)
 
         //call viewmodel method
         viewModel.getYeQuote()
         binding.lifecycleOwner = this
         // Get and Display the loaded quote from SplashFragment
-        binding.quoteTextView.text = viewModel.getYeQuote()
+        binding.quoteTextView.text = viewModel.getYeQuote().value?.quote
 
         binding.quoteButton.setOnClickListener {
             // Get and display more quotes on button click
-            binding.quoteTextView.text = viewModel.getYeQuote()
+            binding.quoteTextView.text = viewModel.getYeQuote().value?.quote
         }
         val view = binding.root
         return view
