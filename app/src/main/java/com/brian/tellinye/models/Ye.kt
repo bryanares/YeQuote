b18ca4ff4f5740ba89284224aeaa2defe089980a
bryanares <bryanares12@gmail.com>
2023-03-14 08:43:04 +0300

    converted model class to entity

--- app/src/main/java/com/brian/tellinye/models/Ye.kt
+++ app/src/main/java/com/brian/tellinye/models/Ye.kt
@@ -1,2 +1,12 @@
-package com.brian.tellinye.models 
+package com.brian.tellinye.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "yeQuotesDb")
+data class Ye(
+    @PrimaryKey
+    val id : Int = 0,
+    val quote: String? = null
+)
