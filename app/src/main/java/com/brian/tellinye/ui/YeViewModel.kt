bc4f025936bb410218fe38f0b4e05d63eef128e3
bryanares <bryanares12@gmail.com>
2023-03-14 08:48:32 +0300

    fetch quote from repository

--- app/src/main/java/com/brian/tellinye/ui/YeViewModel.kt
+++ app/src/main/java/com/brian/tellinye/ui/YeViewModel.kt
@@ -6,27 +6,47 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.brian.tellinye.network.Ye
-import com.brian.tellinye.network.YeApi
+import com.brian.tellinye.models.Ye
+import com.brian.tellinye.repositories.QuotesRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.lang.Exception
 
-class YeViewModel: ViewModel() {
+
+class YeViewModel(private val yeRepository: QuotesRepository) : ViewModel() {
 
     //response variable
-    private val _ye = MutableLiveData<Ye>()
-    val ye : LiveData<Ye> = _ye
+    private val yeQuote = MutableLiveData<Ye>()
+    val yeQuotes: LiveData<Ye> get() = yeQuote
 
+    init {
+        getQuote()
+    }
 
     //function to get the quote
-    fun getYeQuote(): MutableLiveData<Ye> {
-        viewModelScope.launch {
-            try {
-                _ye.value = YeApi.retrofitService.getYe()
-            }catch (e: Exception){
-                Log.e("ViewModel", "Error fetching data")
-            }
+    fun getQuote() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val response = yeRepository.getYeQuote() ?: Ye()
+            Log.d("", "the response is $response")
+            yeQuote.postValue(response)
         }
-        return _ye
     }
+//    fun getYeQuote(): MutableLiveData<Ye> {
+//        viewModelScope.launch {
+//            try {
+//                _ye.value = YeApi.retrofitService.getYe()
+//            }catch (e: Exception){
+//                Log.e("ViewModel", "Error fetching data")
+//            }
+//        }
+//        return _ye
+//    }
 }
+//class YeViewModelFactory(private val yeRepository: QuotesRepository) : ViewModelProvider.Factory {
+//    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+//        if (modelClass.isAssignableFrom(YeViewModel::class.java)) {
+//            @Suppress("UNCHECKED_CAST")
+//            return YeViewModel(yeRepository) as T
+//        }
+//        throw IllegalArgumentException("Unknown ViewModel class")
+//    }
+//}
