eb6493eda36d151a7f851e44cd02f124c9ea54da
bryanares <bryanares12@gmail.com>
2023-03-22 10:05:55 +0300

    implement repository for local caching

--- app/src/main/java/com/brian/tellinye/repositories/QuotesRepository.kt
+++ app/src/main/java/com/brian/tellinye/repositories/QuotesRepository.kt
@@ -1,24 +1,29 @@
 package com.brian.tellinye.repositories
 
+import android.app.Application
 import android.util.Log
-import com.brian.tellinye.database.QuotesDao
+import com.brian.tellinye.database.QuotesDatabase
 import com.brian.tellinye.models.Ye
 import com.brian.tellinye.network.YeApi
 
 
-class QuotesRepository(private val api : YeApi, private val yeDao: QuotesDao) {
+class QuotesRepository(private val application : Application) {
 
     suspend fun getYeQuote() : Ye? {
 
         return try {
             //make network call and insert fetched data into database
-            val quote = api.retrofitService.getYe()
+            val quote = YeApi.retrofitService.getYe()
+            val quotesDatabase = QuotesDatabase.getDatabase(application)
             Log.d("Repo", "Quote is $quote")
-            yeDao.insertQuote(quote)
+
+            val quotesDao = quotesDatabase.quotesDao()
+            quotesDao!!.insertQuote(quote)
+
 
             //extract all saved quotes from db
-            val allQuotes = yeDao.getAllQuotes()
-            Log.d("", "All quotes are $allQuotes")
+            val allQuotes = quotesDao.getAllQuotes()
+            Log.d("All the quotes are", allQuotes.toString())
 
             if (!allQuotes.isNullOrEmpty())
                 allQuotes.first()
--- app/src/main/java/com/brian/tellinye/ui/YeViewModel.kt
+++ app/src/main/java/com/brian/tellinye/ui/YeViewModel.kt
@@ -1,6 +1,7 @@
 package com.brian.tellinye.ui
 
 
+import android.app.Application
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -8,45 +9,30 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.brian.tellinye.models.Ye
 import com.brian.tellinye.repositories.QuotesRepository
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 
-class YeViewModel(private val yeRepository: QuotesRepository) : ViewModel() {
-
+class YeViewModel() : ViewModel() {
     //response variable
     private val yeQuote = MutableLiveData<Ye>()
     val yeQuotes: LiveData<Ye> get() = yeQuote
+    private var yeRepository : QuotesRepository? = null
 
-    init {
-        getQuote()
+    fun setRepo(application: Application){
+        yeRepository = QuotesRepository(application)
     }
 
     //function to get the quote
     fun getQuote() {
-        viewModelScope.launch(Dispatchers.IO) {
-            val response = yeRepository.getYeQuote() ?: Ye()
+        val coroutineExceptionHandler = CoroutineExceptionHandler {_, throwable ->
+            throwable.printStackTrace()
+        }
+        viewModelScope.launch(Dispatchers.IO + coroutineExceptionHandler) {
+            val response = yeRepository!!.getYeQuote() ?: Ye()
             Log.d("", "the response is $response")
             yeQuote.postValue(response)
         }
     }
-//    fun getYeQuote(): MutableLiveData<Ye> {
-//        viewModelScope.launch {
-//            try {
-//                _ye.value = YeApi.retrofitService.getYe()
-//            }catch (e: Exception){
-//                Log.e("ViewModel", "Error fetching data")
-//            }
-//        }
-//        return _ye
-//    }
 }
-//class YeViewModelFactory(private val yeRepository: QuotesRepository) : ViewModelProvider.Factory {
-//    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-//        if (modelClass.isAssignableFrom(YeViewModel::class.java)) {
-//            @Suppress("UNCHECKED_CAST")
-//            return YeViewModel(yeRepository) as T
-//        }
-//        throw IllegalArgumentException("Unknown ViewModel class")
-//    }
-//}
